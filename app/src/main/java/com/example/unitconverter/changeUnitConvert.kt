a55532dfd99ca0ec734512d290ae187973adf331
Aikakarn100597 <59160073@go.buu.ac.th>
2019-10-16 00:15:58 +0700

    third page and navigation

--- app/src/main/java/com/example/unitconverter/changeUnitConvert.kt
+++ app/src/main/java/com/example/unitconverter/changeUnitConvert.kt
@@ -6,9 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import com.example.unitconverter.databinding.FragmentUnitConverterBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,22 +17,14 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class unitConverter : Fragment() {
+class changeUnitConvert : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        val binding = DataBindingUtil.inflate<FragmentUnitConverterBinding>(inflater,R.layout.fragment_unit_converter,container,false)
-
-        binding.confirmButton.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_unitConverter_to_changeUnitConvert)
-        }
-
-        return binding.root
-//        return inflater.inflate(R.layout.fragment_unit_converter, container, false)
+        return inflater.inflate(R.layout.fragment_change_unit_convert, container, false)
     }
 
 
