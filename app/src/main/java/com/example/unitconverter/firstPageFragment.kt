44b73f93b9dede9ad47e0cd79b92304549f22332
Aikakarn100597 <59160073@go.buu.ac.th>
2019-10-15 21:28:31 +0700

    first page navigation and add list of spinner

--- app/src/main/java/com/example/unitconverter/firstPageFragment.kt
+++ app/src/main/java/com/example/unitconverter/firstPageFragment.kt
@@ -1,12 +1,14 @@
 package com.example.unitconverter
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.unitconverter.databinding.FragmentFirstPageBinding
+import kotlinx.android.synthetic.main.fragment_first_page.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,22 @@ class firstPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_page, container, false)
+        val binding = DataBindingUtil.inflate<FragmentFirstPageBinding>(inflater,R.layout.fragment_first_page,container,false)
+
+        binding.lengthButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_firstPageFragment_to_unitConverter)
+        }
+
+        binding.temperatureButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_firstPageFragment_to_unitConverter)
+        }
+
+        binding.weightButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_firstPageFragment_to_unitConverter)
+        }
+
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_first_page, container, false)
     }
 
 
